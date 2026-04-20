477dbca1a60888f527fe32c2a18a3c71760381f7
hs6074 <hs6074@srmist.edu.in>
2026-04-20 08:55:40 +0530

    Added dev code

---
+++ Tic-Tac-Toe-Game/src/main/java/org/example/TicTacToe_uc1.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class TicTacToe_uc1 {
+    static char[][] board = new char[3][3];
+
+    public static void main(String[] args) {
+        initializeBoard();
+        printBoard();
+    }
+
+    static void initializeBoard() {
+
+    }
+
+    static void printBoard() {
+        System.out.println("____________");
+        for (int row = 0; row < 3; row++) {
+        }
+    }
+}
