b7e904f6c019985091abfbdd0a19db4da21a8013
hs6074 <hs6074@srmist.edu.in>
2026-04-29 12:26:17 +0530

    Commit

--- Tic-Tac-Toe-Game/src/main/java/org/example/Main.java
+++ Tic-Tac-Toe-Game/src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
-
 import java.util.Scanner;
 
 public class Main {
+
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
 
     public static void main(String[] args) {
 
@@ -11,15 +16,30 @@ public class Main {
         System.out.print("Enter slot (1-9): ");
         int slot = sc.nextInt();
 
-        System.out.println("Row: " + getRowFromSlot(slot));
-        System.out.println("Column: " + getColFromSlot(slot));
+        int row = getRowFromSlot(slot);
+        int col = getColFromSlot(slot);
+
+        if (isValidMove(row, col)) {
+            System.out.println("Valid move at (" + row + ", " + col + ")");
+        } else {
+            System.out.println("Invalid move!");
+        }
     }
 
+    // UC4 → Slot to Row
     static int getRowFromSlot(int slot) {
         return (slot - 1) / 3;
     }
 
+    // UC4 → Slot to Column
     static int getColFromSlot(int slot) {
         return (slot - 1) % 3;
+    }
+
+    // UC5 → Validation
+    static boolean isValidMove(int row, int col) {
+        return row >= 0 && row < 3 &&
+                col >= 0 && col < 3 &&
+                board[row][col] == '-';
     }
 }
